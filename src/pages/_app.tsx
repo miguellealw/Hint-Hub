19de426ef47312c6df1d7c202305adb4ceb47fcf
Miguel <miguellealw@gmail.com>
2022-12-25 21:53:53 -0600

    Set up Mantine and updated landing page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,19 +1,37 @@
 import { type AppType } from "next/app";
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
+import { MantineProvider } from "@mantine/core";
 
 import { trpc } from "../utils/trpc";
 
 import "../styles/globals.css";
+import Head from "next/head";
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
   pageProps: { session, ...pageProps },
 }) => {
   return (
-    <SessionProvider session={session}>
-      <Component {...pageProps} />
-    </SessionProvider>
+    <>
+      <Head>
+        <title>Page title</title>
+        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+      </Head>
+
+      <MantineProvider
+        withCSSVariables
+        withNormalizeCSS
+        theme={{
+          fontFamily: "Inter, sans-serif",
+          // colorScheme: "dark",
+        }}
+      >
+        <SessionProvider session={session}>
+          <Component {...pageProps} />
+        </SessionProvider>
+      </MantineProvider>
+    </>
   );
 };
 
--- src/server/trpc/router/_app.ts
+++ src/server/trpc/router/_app.ts
@@ -1,10 +1,12 @@
 import { router } from "../trpc";
 import { authRouter } from "./auth";
 import { exampleRouter } from "./example";
+import { hintRouter } from "./hint";
 
 export const appRouter = router({
   example: exampleRouter,
   auth: authRouter,
+  hint: hintRouter
 });
 
 // export type definition of API
