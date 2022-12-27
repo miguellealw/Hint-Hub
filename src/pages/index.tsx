700576469aafe97a6892d9ab34a797e70751cecb
Miguel <miguellealw@gmail.com>
2022-12-27 16:22:04 -0600

    Changed landing page button from "Create Hint" to "Get Started"

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 import { signIn, signOut, useSession } from "next-auth/react";
 import { Container, Header, Button, Title, createStyles, Text } from "@mantine/core";
 import { trpc } from "../utils/trpc";
-import { IconFilePlus, IconLogin } from "@tabler/icons";
+import { IconArrowNarrowRight, IconFilePlus, IconLogin } from "@tabler/icons";
 import Logo from "./components/Logo";
 import MainHeader from "./components/MainHeader";
 import MainLayout from "./components/layouts/MainLayout";
@@ -35,9 +35,9 @@ const Home: NextPage = () => {
         Hint Hub allows you to quickly create and search small notes, or “hints”, on things that may be hard to remember, like keyboard shortcuts, math formulas, or a common process to follow.
       </Text>
 
-      <Button size="md" color="indigo.8" style={{ alignSelf: "center" }} leftIcon={<IconFilePlus size={18} />}>
-        <Link href="/hints">
-          Create Hint
+      <Button size="md" color="indigo.8" style={{ alignSelf: "center" }} rightIcon={<IconArrowNarrowRight size={18} />}>
+        <Link href="/collections">
+           Get Started
         </Link>
       </Button>
     </MainLayout>
