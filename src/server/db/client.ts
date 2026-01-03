080888910eecc956857046353db13b44732499c0
Miguel <miguellealw@gmail.com>
2026-01-02 21:34:07 -0600

    Updated prisma to latest version

--- src/server/db/client.ts
+++ src/server/db/client.ts
@@ -1,4 +1,6 @@
 import { PrismaClient } from "@prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+import pg from "pg";
 
 import { env } from "../../env/server.mjs";
 
@@ -7,9 +9,13 @@ declare global {
   var prisma: PrismaClient | undefined;
 }
 
+const pool = new pg.Pool({ connectionString: env.DATABASE_URL });
+const adapter = new PrismaPg(pool);
+
 export const prisma =
   global.prisma ||
   new PrismaClient({
+    adapter,
     log:
       env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
   });
