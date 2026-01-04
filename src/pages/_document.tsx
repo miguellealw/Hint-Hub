5d1fbf493ef7973ad314a72d18a9869eed2fd651
Miguel <miguellealw@gmail.com>
2026-01-04 00:27:55 -0600

    Fixed zoom issue on mobile

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -9,7 +9,12 @@ export default class _Document extends Document {
   render() {
     return (
       <Html>
-        <Head />
+        <Head>
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
